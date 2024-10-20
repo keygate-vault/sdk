32b20c6aaaaf4698624611487d498959d8ed4cf3
Vicente Bermúdez <vicenteb@web3pro.com>
2024-10-20 00:01:33 -0300

    secp256k1 identity

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ic_agent::{Agent, Identity};
+use ic_agent::{identity::{Secp256k1Identity}, Agent};
 use anyhow::Result;
 
 pub async fn create_agent(url: &str, is_mainnet: bool) -> Result<Agent> {
@@ -9,13 +9,19 @@ pub async fn create_agent(url: &str, is_mainnet: bool) -> Result<Agent> {
     Ok(agent)
 }
 
-pub async fn load_identity(path: &str) -> Result<Identity> {
+pub async fn load_identity(path: &str) -> Result<Secp256k1Identity> {
     // We're going to ignore the PEM file for now
-    let identity = Identity::new
-    Ok(identity)
+    let identity = Secp256k1Identity::from_pem_file(path);
+    match identity {
+        Ok(identity) => Ok(identity),
+        Err(e) => anyhow::bail!("Failed to load identity: {}", e),
+    }
 }
 
-fn main() {
-    let 
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let identity = load_identity("identity.pem").await?;
     println!("Hello, world!");
+    Ok(())
 }
