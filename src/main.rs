1a2cdee14c03644200f03500e4bb019c937c39db
Vicente Bermúdez <vicenteb@web3pro.com>
2024-10-20 00:24:06 -0300

    skeleton ok

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use ic_agent::{identity::{Secp256k1Identity}, Agent};
-use anyhow::Result;
+use ic_agent::{export::Principal, identity::Secp256k1Identity, Agent, Identity};
+use anyhow::{Error, Result};
 
 pub async fn create_agent(url: &str, is_mainnet: bool) -> Result<Agent> {
     let agent = Agent::builder().with_url(url).build()?;
@@ -10,7 +10,6 @@ pub async fn create_agent(url: &str, is_mainnet: bool) -> Result<Agent> {
 }
 
 pub async fn load_identity(path: &str) -> Result<Secp256k1Identity> {
-    // We're going to ignore the PEM file for now
     let identity = Secp256k1Identity::from_pem_file(path);
     match identity {
         Ok(identity) => Ok(identity),
@@ -18,10 +17,38 @@ pub async fn load_identity(path: &str) -> Result<Secp256k1Identity> {
     }
 }
 
+pub struct KeygateClient {
+    agent: Agent,
+}
+
+impl KeygateClient {
+    pub async fn new(identity: Secp256k1Identity, url: &str) -> Result<Self, Error> {
+        let agent = Agent::builder()
+            .with_url(url)
+            .with_identity(identity)
+            .build()?;
+        agent.fetch_root_key().await?;
+        Ok(Self { agent })
+    }
+
+    pub async fn create_wallet(&self) -> Result<(Principal, String)> {
+        panic!("Not implemented");
+    }
+
+    pub async fn get_icp_account(&self, wallet_id: &str) -> Result<String> {
+        panic!("Not implemented");
+    }
+
+    pub async fn execute_transaction(&self, wallet_id: &str, transaction: &str) -> Result<String> {
+        panic!("Not implemented");
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let identity = load_identity("identity.pem").await?;
+    let keygate = KeygateClient::new(identity, "https://ic0.app").await?;
+    
     println!("Hello, world!");
     Ok(())
 }
