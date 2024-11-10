bbc1955b433ec9c61ec0233aefc3bfc8c0062b84
Quino <joaquinballara02@gmail.com>
2024-11-10 14:52:14 -0300

    send_transaction error

--- examples/basic-cli/main.rs
+++ examples/basic-cli/main.rs
@@ -1,13 +1,17 @@
 use candid::Principal;
-use keygate_sdk::{load_identity, KeygateClient};
+use keygate_sdk::{
+    load_identity, KeygateClient, ProposeTransactionArgs, SupportedNetwork, TransactionType,
+};
 use std::process::Command;
 use std::time::Duration;
 use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
-    println!("Welcome to the Keygate Client CLI! \nPlease wait while we create your wallet...\n");
-    sleep(Duration::from_secs(1)).await;
+    println!(
+        "\nWelcome to the Keygate Client CLI! \n\nPlease wait while we create your wallet...\n"
+    );
+    sleep(Duration::from_secs(2)).await;
 
     println!("Loading identity...");
     let identity = load_identity("identity.pem").await.unwrap();
@@ -23,14 +27,34 @@ async fn main() {
     let wallet_id = keygate.create_wallet().await.unwrap();
     println!("Wallet created with ID: {}", wallet_id);
 
-    let output = Command::new("dfx").args(&"ledger transfer <vault_account_id> --amount 100  --memo 1 --network local --identity minter --fee 0".split(' ').collect::<Vec<&str>>())
+    let address = keygate
+        .get_icp_account(&wallet_id.to_string())
+        .await
+        .unwrap();
+
+    let output = Command::new("dfx")
+        .args(&[
+            "ledger",
+            "transfer",
+            &address.to_string(),
+            "--amount",
+            "100",
+            "--memo",
+            "1",
+            "--network",
+            "local",
+            "--identity",
+            "minter",
+            "--fee",
+            "0",
+        ])
         .output()
         .expect("failed to execute process");
 
     if output.status.success() {
         println!("Wallet funded with 100 ICP!");
     } else {
-        eprintln!("Failed to fund wallet with 100 ICP!");
+        eprintln!("Failed to fund wallet: {:?}", output);
         return;
     }
 
@@ -43,20 +67,28 @@ async fn main() {
 
         match input[1] {
             "balance" => {
-                check_balance(keygate.clone(), wallet_id.clone()).await;
+                check_balance(&keygate, wallet_id.clone()).await;
             }
             "transact" => {
-                send_transaction(input[2], input[3]);
+                if input.len() < 4 {
+                    eprintln!("Usage: transact <address> <amount>");
+                    continue;
+                }
+                send_transaction(&keygate, wallet_id.clone(), input[1], input[2]).await;
             }
             "address" => {
-                println!("Your wallet address is: {}", wallet_id.to_string());
+                println!("Your account ID is: {}", address);
             }
-            _ => eprintln!("Unknown wallet command: {}", input[2]),
+            "exit" => {
+                println!("Exiting...");
+                break;
+            }
+            _ => eprintln!("Unknown wallet command: {}", input.join(" ")),
         }
     }
 }
 
-async fn check_balance(keygate: KeygateClient, wallet_id: Principal) {
+async fn check_balance(keygate: &KeygateClient, wallet_id: Principal) {
     print!("Checking balance... ");
     let balance = keygate
         .get_icp_balance(&wallet_id.to_string())
@@ -65,16 +97,23 @@ async fn check_balance(keygate: KeygateClient, wallet_id: Principal) {
     println!("Your balance is: {:?}", balance);
 }
 
-fn send_transaction(keygate: KeygateClient, wallet_id: Principal, address: &str, amount: &str) {
+async fn send_transaction(
+    keygate: &KeygateClient,
+    wallet_id: Principal,
+    address: &str,
+    amount: &str,
+) {
+    print!("Setting up transaction... ");
     let transaction = ProposeTransactionArgs {
-        to: account_id.to_string(),
+        to: address.to_string(),
         token: "icp:native".to_string(),
         transaction_type: TransactionType::Transfer,
         network: SupportedNetwork::ICP,
-        amount: f64::from(0),
+        amount: amount.parse().unwrap(),
     };
+    println!("Sending transaction... ");
     let executed_transaction = keygate
         .execute_transaction(&wallet_id.to_string(), &transaction)
-        .await?;
-    println!("Transaction status: {:?}", executed_transaction);
+        .await;
+    println!("Transaction sent: {:?}", executed_transaction);
 }
